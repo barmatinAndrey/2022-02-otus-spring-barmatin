2bcfe3451e2e74eba7f10ee1af451cfe9ba8a6e3
barmatin <barmatin.andrey@mail.ru>
2022-04-30 02:55:00 +0700

    homework-07 исправление замечания

--- homework-07/src/main/java/ru/barmatin/homework07/repository/CommentRepository.java
+++ homework-07/src/main/java/ru/barmatin/homework07/repository/CommentRepository.java
@@ -1,15 +1,12 @@
 package ru.barmatin.homework07.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.barmatin.homework07.domain.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("select c from Comment c where c.book.id = :bookId")
-    List<Comment> findAllByBookId(@Param("bookId") long bookId);
+    List<Comment> findAllByBookId(long bookId);
 
 }
