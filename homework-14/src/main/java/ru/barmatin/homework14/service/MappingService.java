34baae77b956b81013ae1b2f0556419f1561a8de
barmatin <barmatin.andrey@mail.ru>
2022-06-30 22:22:15 +0700

    interface

--- homework-14/src/main/java/ru/barmatin/homework14/service/MappingService.java
+++ homework-14/src/main/java/ru/barmatin/homework14/service/MappingService.java
@@ -1,7 +1,5 @@
 package ru.barmatin.homework14.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import ru.barmatin.homework14.domain.h2.AuthorH2;
 import ru.barmatin.homework14.domain.h2.BookH2;
 import ru.barmatin.homework14.domain.h2.GenreH2;
@@ -9,50 +7,12 @@ import ru.barmatin.homework14.domain.mongo.AuthorMongo;
 import ru.barmatin.homework14.domain.mongo.BookMongo;
 import ru.barmatin.homework14.domain.mongo.GenreMongo;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+public interface MappingService {
 
-@Service
-public class MappingService {
-    private Long authorCount = 1L;
-    private final HashMap<String, Long> authorMap;
-    private Long genreCount = 1L;
-    private final HashMap<String, Long> genreMap;
+    AuthorH2 authorMongoToH2(AuthorMongo authorMongo);
 
-    @Autowired
-    public MappingService() {
-        authorMap = new HashMap<>();
-        genreMap = new HashMap<>();
-    }
+    GenreH2 genreMongoToH2(GenreMongo genreMongo);
 
-    public AuthorH2 authorMongoToH2(AuthorMongo authorMongo){
-        AuthorH2 authorH2 = new AuthorH2(authorCount, authorMongo.getSurname(), authorMongo.getName(), authorMongo.getPatronym());
-        authorMap.put(authorMongo.getId(), authorH2.getId());
-        authorCount++;
-        return authorH2;
-    }
+    BookH2 bookMongoToH2(BookMongo bookMongo);
 
-    public GenreH2 genreMongoToH2(GenreMongo genreMongo){
-        GenreH2 genreH2 = new GenreH2(genreCount, genreMongo.getName());
-        genreMap.put(genreMongo.getId(), genreH2.getId());
-        genreCount++;
-        return genreH2;
-    }
-
-    public BookH2 bookMongoToH2(BookMongo bookMongo){
-        AuthorH2 authorH2 = new AuthorH2(
-                authorMap.get(bookMongo.getAuthorMongo().getId()),
-                bookMongo.getAuthorMongo().getSurname(),
-                bookMongo.getAuthorMongo().getName(),
-                bookMongo.getAuthorMongo().getPatronym());
-        List<GenreH2> genreH2List = new ArrayList<>();
-        for (GenreMongo genreMongo: bookMongo.getGenreMongoList()) {
-            genreH2List.add(new GenreH2(
-                    genreMap.get(genreMongo.getId()),
-                    genreMongo.getName()
-            ));
-        }
-        return new BookH2(bookMongo.getName(), authorH2, genreH2List);
-    }
 }
