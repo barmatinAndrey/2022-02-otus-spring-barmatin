917c4220255f0cd23a6d6aaa98af577184bb7992
barmatin <barmatin.andrey@mail.ru>
2022-07-15 14:58:51 +0700

    docker-compose

--- homework-17/src/main/java/ru/barmatin/homework17/Homework17Application.java
+++ homework-17/src/main/java/ru/barmatin/homework17/Homework17Application.java
@@ -10,5 +10,6 @@ public class Homework17Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Homework17Application.class, args);
 	}
+//	docker run --detach --env="POSTGRES_PASSWORD=password" --publish 6603:3306 postgres
 
 }
