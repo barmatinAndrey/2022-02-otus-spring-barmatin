d49501121f9f9dbfbc50c702eab610d5972caf9d
barmatin <barmatin.andrey@mail.ru>
2022-08-12 17:42:06 +0700

    com.mysql.cj.jdbc.Driver

---
+++ collective-blog/src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+drop table if exists blog_users;
+create table blog_users(
+    id int,
+    username varchar(255),
+    password varchar(255),
+    role varchar(255),
+    surname varchar(255),
+    name varchar(255),
+    email varchar(255),
+    primary key(id)
+);
