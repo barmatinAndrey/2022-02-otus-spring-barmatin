19526f9a25f8a2d6d209d2b0b8beeca3e725ad61
barmatin <barmatin.andrey@mail.ru>
2022-08-30 17:17:46 +0700

    security test

--- collective-blog/src/test/java/ru/barmatin/collectiveblog/security/SecurityTest.java
+++ collective-blog/src/test/java/ru/barmatin/collectiveblog/security/SecurityTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.barmatin.collectiveblog.controller.PageController;
 import ru.barmatin.collectiveblog.controller.rest.PostController;
 import ru.barmatin.collectiveblog.service.bloguser.BlogUserService;
 import ru.barmatin.collectiveblog.service.post.PostService;
@@ -15,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
-@WebMvcTest(PostController.class)
+@WebMvcTest({PostController.class, PageController.class})
 @Import({UserDetailsServiceImpl.class, SpringSecurityConfig.class})
 class SecurityTest {
 
@@ -34,6 +35,12 @@ class SecurityTest {
     @Test
     void editPage() throws Exception {
         mockMvc.perform(get("/post-edit/new")).andExpect(status().is3xxRedirection());
+    }
+
+    @WithMockUser(authorities = {"ADMIN", "USER"})
+    @Test
+    void editPageUser() throws Exception {
+        mockMvc.perform(get("/post-edit/new")).andExpect(status().isOk());
     }
 
 }
