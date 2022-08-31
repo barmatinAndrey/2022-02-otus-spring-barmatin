37ce2e04eae2e6b23f5d77a925c0bbcff177f3a1
barmatin <barmatin.andrey@mail.ru>
2022-08-31 22:34:57 +0700

    final

--- collective-blog/src/main/java/ru/barmatin/collectiveblog/repository/PostCommentRepository.java
+++ collective-blog/src/main/java/ru/barmatin/collectiveblog/repository/PostCommentRepository.java
@@ -13,6 +13,9 @@ public interface PostCommentRepository extends JpaRepository<PostComment, Long> 
 //    @Query("select t.content from PostComment t where t.post.id = :postId")
 //    List<String> findAllContentByPostId(@Param("postId") long postId);
 
+
+    //TODO:Это размножит юзера и пост по количеству комментов,
+    // т.е. если у поста 100 комментариев, то его контент приедет в размере умноженном на 100
     @EntityGraph(attributePaths = {"post", "blogUser"})
     List<PostComment> findAllByPostId(@Param("postId") long postId);
 
